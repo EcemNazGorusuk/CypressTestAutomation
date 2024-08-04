e1fe60c8ff6bbee04ed6fd7332cc47c0725976e2
Ecem <ecemnazgorusuk@gmail.com>
2024-08-04 19:35:30 +0300

    Login test updated

--- cypress/e2e/user-tests/login-tests.cy.js
+++ cypress/e2e/user-tests/login-tests.cy.js
@@ -6,7 +6,9 @@ describe("Login Test", () => {
   //beforeEach(() => {});
 
   it("Login Successful", () => {
-    cy.visit("https://www.netflix.com/login");
+    cy.visit("https://www.netflix.com/login"); //test edilecek siteye yönlendirme işlemi
+    cy.get("[name='userLoginId']").type("ecemnazgorusuk@gmail.com"); //e-posta input alanı alınır ve input alanı doldurulur
+    cy.get("[data-uia='password-field']").type("ecemnaz12345"); // name yerine data-uia alanı da verebiliriz
   });
-  
+
 });
