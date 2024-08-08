dfa04e5d5f2e650e4b36e05112e1f2a6f3527436
Ecem <ecemnazgorusuk@gmail.com>
2024-08-08 23:24:49 +0300

    The Cypress Cloud has been integrated.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "szc1qg",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
